2b6a0f8829160424de9de3640dca7b6a999409c9
Jonathan Wong <chi.jonathanwong@gmail.com>
2018-02-13 01:20:55 -0800

    Add Valid URLs

--- scraper.py
+++ scraper.py
@@ -1,19 +1,11 @@
 from bs4 import BeautifulSoup
+import requests 
 
-html_doc = """
-<html><head><title>The Dormouse's story</title></head>
-<body>
-<p class="title"><b>The Dormouse's story</b></p>
+#Loops through the specified page numbers for the analsis: pages for prior to December 1, 2014 
+for i in range(6, 31):
+    url = "http://www.newyorksocialdiary.com/party-pictures?page=%d" %i 
+    #response = requests.get(url) 
+    print(url)
 
-<p class="story">Once upon a time there were three little sisters; and their names were
-<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
-<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
-<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
-and they lived at the bottom of a well.</p>
-
-<p class="story">...</p>
-"""
-
-soup = BeautifulSoup(html_doc, 'html.parser')
-
-print(soup.prettify()) 
+#soup = BeautifulSoup(response.text)
+#print(soup.prettify())
